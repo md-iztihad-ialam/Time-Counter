b4bbd5162dd183677b59e4fca0d7ff1f7d9da8b9
Md. Iztihad Islam <iiztihad@gmail.com>
2024-12-04 02:07:23 +0600

    added stop watch- app version: 3.0.0.0

--- src/components/Pages/CountDown.jsx
+++ src/components/Pages/CountDown.jsx
@@ -1,7 +1,40 @@
-function CountDown(){
+function Countdown(){
     return(
-        <div>Countdown</div>
+        <div className="w-[100%] h-[80dvh] bg-[#1c1c1c] flex justify-center items-center flex-col gap-5">
+            <h1 className="text-4xl text-white font-mono">Just An Ordinary Stop Watch</h1>
+            <h3 className="text-2xl text-white font-mono">RUN THE TIME</h3>
+                <div className="flex gap-5">
+
+                    <input 
+                        type="number"
+                        className="border-none rounded-xl bg-[#7d7d7d] p-[15px] w-[150px] text-white text-xl hover:bg-[#b0b0b0] hover:text-black"
+                    />
+
+                    <input 
+                        type="number"
+                        className="border-none rounded-xl bg-[#7d7d7d] p-[15px] w-[150px] text-white text-xl hover:bg-[#b0b0b0] hover:text-black"
+                    />
+
+                    <input 
+                        type="number"
+                        className="border-none rounded-xl bg-[#7d7d7d] p-[15px] w-[150px] text-white text-xl hover:bg-[#b0b0b0] hover:text-black"
+                    />
+
+                </div>
+
+                <div className="flex w-[500px] justify-around text-white text-4xl">
+                    <p>Hours</p>
+                    <p>Minutes</p>
+                    <p>Seconds</p>
+                </div>
+
+                <button
+                    className="border-none rounded-full bg-[#7d7d7d] p-[10px] h-[80px] w-[80px] text-white text-xl hover:bg[#b0b0b0] hover:text-black "
+                >
+                    Start
+                </button>
+        </div>
     );
 }
 
-export default CountDown;
+export default Countdown;
--- src/components/Pages/StopWatch.jsx
+++ src/components/Pages/StopWatch.jsx
@@ -1,6 +1,128 @@
+import { useEffect, useRef, useState } from "react";
+
 function StopWatch(){
+
+    const [second, setSecond] = useState(0);
+    const [minute, setMinute] = useState(0);
+    const [hour, setHour] = useState(0);
+    const id = useRef(null);
+
+    function handleStart(){
+        if(!id.current){
+            id.current = setInterval(() => {
+                setSecond((second) => {
+                    const newSecond = second+1;
+                    if(newSecond == 5){
+                        setSecond(0);
+                        setMinute((minute) => {
+                            const newMinute = minute+1;
+                            if(newMinute == 5){
+                                setMinute(0);
+                                setHour(hour+1);
+                            }
+
+                            return newMinute;
+                        })
+                    }
+                    return newSecond;
+                });
+            }, 1000);
+        }
+    }
+
+    function handleStop(){
+        clearInterval(id.current);
+        
+    }
+
+    function handleReset(){
+        id.current=null;
+        setSecond(0);
+        setMinute(0);
+        setHour(0);
+    }
+
+    function handleResume(){
+        if(id.current){
+            id.current = setInterval(() => {
+                setSecond((second) => {
+                    const newSecond = second+1;
+                    if(newSecond == 5){
+                        setSecond(0);
+                        setMinute((minute) => {
+                            const newMinute = minute+1;
+                            if(newMinute == 5){
+                                setMinute(0);
+                                setHour(hour+1);
+                            }
+
+                            return newMinute;
+                        })
+                    }
+                    return newSecond;
+                });
+            }, 1000);
+        }
+    }
+
+
     return(
-        <div>Stop watch</div>
+        <div className="w-[100%] h-[80dvh] bg-[#1c1c1c] flex justify-center items-center flex-col gap-5">
+            <h1 className="text-4xl text-white font-mono">Just An Ordinary Stop Watch</h1>
+            <h3 className="text-2xl text-white font-mono">RUN THE TIME</h3>
+            <div className="flex gap-5">
+
+                <div 
+                    className="w-[200px] h-[100px] rounded-xl bg-[#7d7d7d] flex justify-center items-center text-5xl   text-white     font-mono"
+                >
+                    {hour}
+                </div>
+
+                <div 
+                    className="w-[200px] h-[100px] rounded-xl bg-[#7d7d7d] flex justify-center items-center text-5xl text-white font-mono"
+                >
+                    {minute}
+                </div>
+
+                <div 
+                    className="w-[200px] h-[100px] rounded-xl bg-[#7d7d7d] flex justify-center items-center text-5xl text-white font-mono"
+                >
+                    {second}
+                </div>
+
+            </div>
+
+            <div className="flex gap-8">
+                <button
+                    className="border-none rounded-full bg-[#7d7d7d] p-[10px] h-[80px] w-[80px] text-white text-xl hover:bg[#b0b0b0]hover:text-black "
+                    onClick={handleStart}
+                >
+                    Start
+                </button>
+
+                <button
+                    className="border-none rounded-full bg-[#7d7d7d] p-[10px] h-[80px] w-[80px] text-white text-xl hover:bg[#b0b0b0]hover:text-black "
+                    onClick={handleStop}
+                >
+                    Stop
+                </button>
+
+                <button
+                    className="border-none rounded-full bg-[#7d7d7d] p-[10px] h-[80px] w-[80px] text-white text-xl hover:bg[#b0b0b0]hover:text-black "
+                    onClick={handleResume}
+                >
+                    Resume
+                </button>
+
+                <button
+                    className="border-none rounded-full bg-[#7d7d7d] p-[10px] h-[80px] w-[80px] text-white text-xl hover:bg[#b0b0b0]hover:text-black "
+                    onClick={handleReset}
+                >
+                    Reset
+                </button>
+            </div>
+
+        </div>
     );
 }
 
